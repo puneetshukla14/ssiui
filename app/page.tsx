c3328c9ff73e090cca3d9f2ceff277ac8fd43813
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-07-18 08:29:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "@/components/Header";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
 import ProductShowcase from "./components/ProductShowcase";
 import ShotOnSection from "./components/ShotOnSection";
 import InnovationSection from "./components/InnovationSection";
